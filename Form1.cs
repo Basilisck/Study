75989b623a86e7a0a92e52b39f03808b019197d7
Konstantin Chepurko <kostya.chepurko1234@mail.ru>
2020-10-20 09:47:02 +0300

    1

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            n = Convert.ToInt32(textBox1.Text);
+            for (i=0;i<n;i++)
+            {
+                int a;
+                a = n % 10;
+                if (a==3)
+                {
+                    int b=0;
+                    b = b + a;
+                }
+            }
+        }
+    }
+}
