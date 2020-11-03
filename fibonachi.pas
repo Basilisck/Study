6867eea91efec116bd1abcb2f5a988a2dfd976f4
Chepurko Konstantin <kostya.chepurko1234552@gmail.com>
2020-11-03 10:04:18 +0600

    SecondFix

--- fibonachi.pas
+++ fibonachi.pas
@@ -1,12 +1,16 @@
 program fibonachi;
-var a, b, c:integer;
+var a, b, c, c1, c2, i:integer;
 begin
   c:=1;
+  c1:=1;
+  c2:=0;
   writeln('Введите колличество элементов чисел Фибоначи');
   readln(a);
   for b:=0 to a do
     begin
-      c:=c+2;
+      c:=c1;
+      c1:=c2+c1;
+      c2:=c;
       write(c,' ')
     end;
   readln;
