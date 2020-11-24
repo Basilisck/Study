aa21236fb9b606b494e513fae37aac50f0654ebf
Chepurko Konstantin <kostya.chepurko1234552@gmail.com>
2020-11-24 09:49:24 +0600

    Test

--- GitIgnore/src/GitIgnore.java
+++ GitIgnore/src/GitIgnore.java
@@ -2,17 +2,12 @@ public class GitIgnore
 {
 	public static void main(String[] args) 
 	{
-		int a=4;
+		int a=4, i, c=0;
 		
-		System.out.println("4 * 1 = " + a*1);
-		System.out.println("4 * 2 = " + a*2);
-		System.out.println("4 * 3 = " + a*3);
-		System.out.println("4 * 4 = " + a*4);
-		System.out.println("4 * 5 = " + a*5);
-		System.out.println("4 * 6 = " + a*6);
-		System.out.println("4 * 7 = " + a*7);
-		System.out.println("4 * 8 = " + a*8);
-		System.out.println("4 * 9 = " + a*9);
-		System.out.println("4 * 10 = " + a*10);
+		for (i = 1;i < 11;i++) 
+		{
+			c = c + a;
+			System.out.println(c);
+		}
 	}
 }
